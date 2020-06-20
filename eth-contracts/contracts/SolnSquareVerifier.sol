0e96f968e19043b4ea494108eff417ddf09bf363
Imdad15 <imdad92@live.com>
2020-06-20 14:44:53 +0800

    Implement SolnSquareVerifier

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,39 +1,62 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import "./ERC721Mintable.sol";
+import "./Verifier.sol";
 
+contract SolnSquareVerifier is CustomERC721Token {
+    Verifier verifier;
 
+    struct Solution{
+        uint256 index;
+        address sender;
+    }
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+    Solution[] solutions;
+    mapping(bytes32 => Solution) uniqueSolutions;
+    uint256 counter = 0;
 
+    event SolutionAdded(bytes32 key, address sender);
 
+    constructor(string memory name, string memory symbol) CustomERC721Token(name, symbol) public {
+        verifier = new Verifier();
+    }
+    //, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") public {}
 
-// TODO define a solutions struct that can hold an index & an address
+    /**
+     * @dev function to add the solutions to the array and emit the event
+     */
+    function addSolution(
+                            uint[2] memory a,
+                            uint[2][2] memory b,
+                            uint[2] memory c,
+                            uint[2] memory input
+                        )
+    public {
+        bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
+        require(uniqueSolutions[key].sender==address(0), "Solution is not unique");
+        counter = counter + 1;
+        uniqueSolutions[key] = Solution({index: counter, sender: msg.sender});
 
+        emit SolutionAdded(key, msg.sender);
+    }
 
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
+    /**
+     * @dev function to mint new NFT only after the solution has been verified
+     */
+    function verifiedMint(
+                            address to,
+                            uint256 tokenId,
+                            uint[2] memory a,
+                            uint[2][2] memory b,
+                            uint[2] memory c,
+                            uint[2] memory input
+                        )
+    public returns (bool) {
+        require((verifier.verifyTx(a, b, c, input)), "Verification failed");
+        addSolution(a, b, c, input);
+        super.mint(to, tokenId);
+    }
+}
 
 
 
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,35 @@
+var SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract('SolnSquareVerifier', accounts => {
+
+    const account_one = accounts[0];
+    const account_two = accounts[1];
+    let proof = require('./proof');
+
+    describe('test contract SolnSquareVerifier', function () {
+        beforeEach(async function () {
+            this.contract = await SolnSquareVerifier.new("Bitland", "BTL", { from: account_one });
+        })
+
+        it('Test if a new solution can be added for contract', async function () {
+            const { proof: { a, b, c }, inputs: input } = proof;
+            let result = await this.contract.addSolution(a, b, c, input, {from: account_two});
+
+            assert.equal(result.logs.length, 1, "No events were triggered.");
+        });
+
+        // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+        it('Test if an ERC721 token can be minted for contract', async function () {
+            const { proof: { a, b, c }, inputs: input } = proof;
+            let totalSupply = await this.contract.totalSupply.call();
+            //console.log(`variables -  ${a}, ${b}, ${c}, ${input}`)
+            await this.contract.verifiedMint(account_two, 5, a, b, c, input, {from: account_one});
+            let newTotalSupply = (await this.contract.totalSupply.call()).toNumber();
+
+            assert.equal(totalSupply + 1, newTotalSupply, "Invalid proof result");
+        })
+
+    });
+})
